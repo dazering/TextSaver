00202cf0b672473e07b00920a73720a218b10904
dazering <47257427+dazering@users.noreply.github.com>
2020-12-04 20:03:26 +0300

    Added interface and realitation TextDbRepository

---
+++ TextSaverWebApp/Repository/ITextDbRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TextSaverWebApp.Models;
+
+namespace TextSaverWebApp.Repository
+{
+    public interface ITextDbRepository
+    {
+        Task AddTextAsync(string text);
+        Task AddTextAsync(TextEntity text);
+        Task<TextEntity> GetTextAsync(int id);
+        Task<IEnumerable<TextEntity>> GetLastTextsAsync(int count);
+    }
+}
---
+++ TextSaverWebApp/Repository/TextDbRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using TextSaverWebApp.DataBase;
+using TextSaverWebApp.Models;
+
+namespace TextSaverWebApp.Repository
+{
+    public class TextDbRepository : ITextDbRepository
+    {
+        private TextDbContext context;
+
+        public TextDbRepository(TextDbContext cntx)
+        {
+            context = cntx;
+        }
+
+        public async Task AddTextAsync(string text)
+        {
+            var partedText = new TextEntity();
+            partedText.SplitText(text);
+            await context.TextEntities.AddAsync(partedText);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task AddTextAsync(TextEntity text)
+        {
+            await context.TextEntities.AddAsync(text);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<TextEntity> GetTextAsync(int id)
+        {
+            var text = await context.TextEntities.Include(t => t.PartedTexts).FirstOrDefaultAsync(t => t.Id == id);
+            return text;
+        }
+
+        public async Task<IEnumerable<TextEntity>> GetLastTextsAsync(int count)
+        {
+            var texts = await context.TextEntities.Include(t => t.PartedTexts).OrderByDescending(t => t.Id).Take(count).ToListAsync();
+            return texts;
+        }
+    }
+}
