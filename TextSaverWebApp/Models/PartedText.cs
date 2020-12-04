fb4bcd2787eddd9bf104161725abdfdf336b8f36
dazering <47257427+dazering@users.noreply.github.com>
2020-12-04 20:02:07 +0300

    Added models

---
+++ TextSaverWebApp/Models/PartedText.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TextSaverWebApp.Models
+{
+    public class PartedText
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int PartNumber { get; set; }
+        public TextEntity TextEntity { get; set; }
+    }
+}
---
+++ TextSaverWebApp/Models/TextEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextSaverWebApp.Models
+{
+    public class TextEntity
+    {
+        public int Id { get; set; }
+        public ICollection<PartedText> PartedTexts { get; private set; }
+
+        public string GetFullText()
+        {
+            return string.Join("",PartedTexts.OrderBy(t => t.PartNumber).SelectMany(s => s.Text));
+        }
+
+        public void SplitText(string str)
+        {
+            StringBuilder sb = new StringBuilder(str);
+
+            PartedTexts = sb.Replace(". ", ". |").Replace("! ", "! |").Replace("? ", "? |").Replace($"{Environment.NewLine}", $"{Environment.NewLine}|").ToString().Split("|").Select((s, i) => new PartedText() { Text = s, PartNumber = i }).ToList();
+        }
+    }
+}
