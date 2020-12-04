f3c9b6ae22007282aee1250f48972e9c12362a19
dazering <47257427+dazering@users.noreply.github.com>
2020-12-04 20:02:34 +0300

    Added TextDbContext

---
+++ TextSaverWebApp/DataBase/TextDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TextSaverWebApp.Models;
+
+namespace TextSaverWebApp.DataBase
+{
+    public class TextDbContext : DbContext
+    {
+        public TextDbContext(DbContextOptions<TextDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TextEntity> TextEntities { get; set; }
+        public DbSet<PartedText> PartedTexts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PartedText>().Property(p=>p.Text).HasColumnType("NVARCHAR(500)");
+        }
+    }
+}
